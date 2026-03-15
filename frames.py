f7e2e5e5033bf0a758acb71d883566fe995d2d3f
nguyenthanhcong0410 <ngthanhcong0410@gmail.com>
2026-03-15 16:19:38 +0700

    edited

--- frames.py
+++ frames.py
@@ -10,6 +10,10 @@ if not os.path.exists(folder):
 
 cap = cv2.VideoCapture(raw_video)
 
+frame_in_second = cap.get(cv2.CAP_PROP_FPS)
+interval = int(frame_in_second) #tui để đại ở đây là mỗi frame cách nhau 1s nha, mốt có data của hưng rồi thì đổi sau cũng được
+
+save_count = 0
 frame_count = 0
 
 while True:
@@ -18,12 +22,14 @@ while True:
     if not ret:
         break
     
-    frame_name = os.path.join(folder, f"frame_{frame_count}.jpg")
+    if frame_count % interval == 0:
+        save_frame_name = os.path.join(folder, f"frame_{save_count}.jpg")
+        cv2.imwrite(save_frame_name, frame)
+        save_count += 1
     
-    #save frame as image
-    cv2.imwrite(frame_name, frame)
     frame_count += 1
+    
 
 cap.release()
 
-print(f"Done extracting {frame_count} frames")
+print(f"Done {save_count} frames")
